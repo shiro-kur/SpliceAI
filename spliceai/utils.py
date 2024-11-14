c757492ad61490f04b5771495bec78db4091043f
Ryo Kurosawa <90682107+shiro-kur@users.noreply.github.com>
2024-11-14 16:11:27 +0900

    Update utils.py repress output log (verbose=0)

--- spliceai/utils.py
+++ spliceai/utils.py
@@ -155,8 +155,8 @@ def get_delta_scores(record, ann, dist_var, mask):
                 x_ref = x_ref[:, ::-1, ::-1]
                 x_alt = x_alt[:, ::-1, ::-1]
 
-            y_ref = np.mean([ann.models[m].predict(x_ref) for m in range(5)], axis=0)
-            y_alt = np.mean([ann.models[m].predict(x_alt) for m in range(5)], axis=0)
+            y_ref = np.mean([ann.models[m].predict(x_ref, verbose=0) for m in range(5)], axis=0)
+            y_alt = np.mean([ann.models[m].predict(x_alt, verbose=0) for m in range(5)], axis=0)
 
             if strands[i] == '-':
                 y_ref = y_ref[:, ::-1]
